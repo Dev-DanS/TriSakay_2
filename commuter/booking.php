574030ebf771a41c8085aebed6e3dccee1a84e55
Dan <146542914+Dev-DanS@users.noreply.github.com>
2023-10-08 13:32:34 +0800

    Updated Booking | 10/8/2023
    v 2.0

--- commuter/booking.php
+++ commuter/booking.php
@@ -15,10 +15,14 @@ session_start();
     <link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css" />
     <script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
+    <style>
+        .custom-btn {
+            display: none;
+        }
+    </style>
 </head>
 
 <body>
-
     <?php
     include('../php/navbar_commuter.php');
     ?>
@@ -30,28 +34,60 @@ session_start();
 
     <div id="map" style="width: 100%; height: 50vh;"></div>
     <div class="locations">
-        <h5>Add a Pickup Point</h5>
-        <h5>Add a Drop-off Point</h5>
+        <p id="pickup-address">Add a Pickup Point</p>
+        <p>Add a Drop-off Point</p>
     </div>
 
-    
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6 mb-2">
+                <button type="submit" class="btn btn-default custom-btn" id="confirm-btn">
+                    Confirm
+                </button>
+            </div>
+            <div class="col-md-6 mb-2">
+                <button type="submit" class="btn btn-default custom-btn" id="undo-btn">
+                    Undo
+                </button>
+            </div>
+        </div>
+    </div>
 
     <script>
         var map = L.map('map', {
-            zoomControl: false
+            zoomControl: false,
+            doubleClickZoom: false // Disable double-click zoom
         }).setView([0, 0], 14);
 
         L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
             attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         }).addTo(map);
 
-        var marker = L.marker([0, 0]).addTo(map).bindPopup('You are here').openPopup();
+        var blueMarkerIcon = L.icon({
+            iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-blue.png',
+            iconSize: [25, 41],
+            iconAnchor: [12, 41],
+            popupAnchor: [1, -34]
+        });
+
+        var greenMarkerIcon = L.icon({
+            iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png',
+            iconSize: [25, 41],
+            iconAnchor: [12, 41],
+            popupAnchor: [1, -34]
+        });
+
+        var blueMarker = L.marker([0, 0], {
+            icon: blueMarkerIcon
+        }).addTo(map).bindPopup('You are here').openPopup();
+
+        var pickupMarker = null; // Initialize pickup marker variable
 
         function updateLocation(position) {
             var latitude = position.coords.latitude;
             var longitude = position.coords.longitude;
 
-            marker.setLatLng([latitude, longitude]).update();
+            blueMarker.setLatLng([latitude, longitude]).update();
             map.setView([latitude, longitude], 14);
         }
 
@@ -64,6 +100,7 @@ session_start();
 
         const searchInput = document.getElementById("search-input");
         const searchButton = document.getElementById("search-button");
+        const pickupAddress = document.getElementById("pickup-address"); // Add the ID to your <h4> element
 
         searchInput.addEventListener("keyup", function (event) {
             if (event.keyCode === 13) {
@@ -82,7 +119,38 @@ session_start();
                     console.log(error);
                 });
         });
+
+        // Function to handle double-click events
+        function handleDoubleClick(event) {
+            var latlng = event.latlng; // Get the clicked coordinates
+
+            // Remove existing pickup marker if it exists
+            if (pickupMarker) {
+                map.removeLayer(pickupMarker);
+            }
+
+            pickupMarker = L.marker(latlng, {
+                icon: greenMarkerIcon
+            }).addTo(map).bindPopup('Pickup point').openPopup();
+
+            // Get the address using reverse geocoding
+            axios.get("https://nominatim.openstreetmap.org/reverse?lat=" + latlng.lat + "&lon=" + latlng.lng + "&format=json&limit=1")
+                .then(function (response) {
+                    var address = response.data.display_name;
+                    pickupAddress.textContent = "Pickup Point: " + address; // Update the <h4> content
+                })
+                .catch(function (error) {
+                    console.log(error);
+                });
+        }
+
+        // Add double-click event listener to the map
+        map.on('dblclick', handleDoubleClick);
     </script>
+
+
+
+
 </body>
 
 </html>
